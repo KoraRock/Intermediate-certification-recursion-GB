a29f241cfac5f50a3044b2271abc4e2510069ed7
KoraRock <sorokina1mari@gmail.com>
2024-07-06 14:02:07 +0300

    выполнена 1 задача

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 1: Задайте значения M и N.
+//Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+//Использовать рекурсию, не использовать циклы.
+
+void MToN(int N, int M)
+{
+    if (N > M-1)
+    {
+        MToN(N-1, M);
+        Console.Write (N + " ");
+    }
+}
+void NToM(int N, int M)
+{
+    if (N < M+1)
+    {
+        NToM(N+1, M);
+        Console.Write (N + " ");
+    }
+}
+// Проверка работоспособности рекурсивных функций (без введения чисел от пользователя):
+// NToM(5, 10);
+// Console.WriteLine(' ');
+// MToN(6, 3);
+
+System.Console.WriteLine("Введите M: " );
+int M = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (M > N) // Проверка условий вызова рекурсивных функций после введения различных чисел от пользователя
+{
+    NToM(N, M);
+}
+else
+{
+    MToN(N, M);
+}
