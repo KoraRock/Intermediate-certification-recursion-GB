7a1cd5e04f1182230ff7f454c9882ea4b4a7e017
KoraRock <sorokina1mari@gmail.com>
2024-07-07 00:20:29 +0300

    выполнена 3 задача

---
+++ Task_3/Program.cs
@@ -0,0 +1,37 @@
+﻿//Задача 3: Задайте произвольный массив. Выведите его элементы, начиная с конца. 
+//Использовать рекурсию, не использовать циклы.
+
+void PrintArrayRec(int[] array, int index = 0)
+{
+    int size = array.Length;
+    if (index < size)
+    {
+        PrintArrayRec(array, index + 1);
+        Console.Write (array[index] + " ");
+    }
+}
+int[] CreateArray(string args)
+{
+    int[] numbers;
+    if (args.Length >= 1)
+    {
+        string[] Elements = args.Split(' ');
+        numbers = new int[Elements.Length];
+        for (int j = 0; j < Elements.Length; j++)
+                {
+                    if (int.TryParse(Elements[j], out int result))
+                    {
+                        numbers[j] = result;
+                    }
+                }
+        return numbers;
+
+    }
+    return numbers = [0];
+    
+}
+
+Console.WriteLine("Введите значения параметров массива через пробел:");
+string str = Console.ReadLine();
+
+PrintArrayRec(CreateArray(str));
--- Task_3/obj/Debug/net8.0/Task_3.AssemblyInfo.cs
+++ Task_3/obj/Debug/net8.0/Task_3.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Task_1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Task_3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Task_1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Task_1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b60178ee394414d6bc3732f54ab9f4b062d7223")]
+[assembly: System.Reflection.AssemblyProductAttribute("Task_3")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Task_3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
